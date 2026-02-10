c2227fdbbcf2e8cf43df00f1eae6af14c630c105
João Victor <joao.dev06@gmail.com>
2026-02-10 00:23:44 -0300

    feat: adiciona a entidade Barber e relacionamento com User

---
+++ src/main/java/br/com/barbearia_agendamentos_api/domain/entity/Barber.java
@@ -0,0 +1,26 @@
+package br.com.barbearia_agendamentos_api.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "barbers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Barber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean ativo;
+
+}
