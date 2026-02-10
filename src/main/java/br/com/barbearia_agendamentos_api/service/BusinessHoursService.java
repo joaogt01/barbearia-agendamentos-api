9935154bacdbed06a6d135d41ed95b54dd0bb8af
João Victor <joao.dev06@gmail.com>
2026-02-10 20:18:16 -0300

    feat: adiciona BusinessHoursService

--- src/main/java/br/com/barbearia_agendamentos_api/service/BusinessHoursService.java
+++ src/main/java/br/com/barbearia_agendamentos_api/service/BusinessHoursService.java
@@ -1,4 +1,28 @@
 package br.com.barbearia_agendamentos_api.service;
 
+import br.com.barbearia_agendamentos_api.domain.entity.BusinessHours;
+import br.com.barbearia_agendamentos_api.repository.BusinessHoursRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class BusinessHoursService {
+
+    private final BusinessHoursRepository businessHoursRepository;
+
+    public BusinessHours save(BusinessHours hours){
+        return businessHoursRepository.save(hours);
+    }
+
+    public boolean isOpen(LocalDateTime dateTime){
+        return businessHoursRepository.findByDiaSemana(dateTime.getDayOfWeek())
+                .map(h ->
+                        !dateTime.toLocalTime().isBefore(h.getHoraAbertura()) &&
+                        !dateTime.toLocalTime().isAfter(h.getHoraFechamento())
+                )
+                .orElse(false);
+    }
 }
