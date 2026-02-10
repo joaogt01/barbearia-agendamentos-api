e291627d66733304d14e60993f6ca7aee0b03ab7
João Victor <joao.dev06@gmail.com>
2026-02-10 13:02:23 -0300

    feat: adiciona ScheduleBlockRepository

---
+++ src/main/java/br/com/barbearia_agendamentos_api/repository/ScheduleBlockRepository.java
@@ -0,0 +1,13 @@
+package br.com.barbearia_agendamentos_api.repository;
+
+import br.com.barbearia_agendamentos_api.domain.entity.ScheduleBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ScheduleBlockRepository extends JpaRepository<ScheduleBlock, Long> {
+
+    List<ScheduleBlock> findBarberIdAndData(Long barberId, LocalDate data);
+
+}
