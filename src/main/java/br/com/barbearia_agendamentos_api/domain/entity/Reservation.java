bd8065a8b5d35b627220dccd63633a92aee09efb
João Victor <joao.dev06@gmail.com>
2026-02-10 01:12:26 -0300

    feat: adiciona entidade Reservation

---
+++ src/main/java/br/com/barbearia_agendamentos_api/domain/entity/Reservation.java
@@ -0,0 +1,56 @@
+package br.com.barbearia_agendamentos_api.domain.entity;
+
+import br.com.barbearia_agendamentos_api.domain.enums.ReservationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private User cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "barber_id", nullable = false)
+    private Barber barber;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(nullable = false)
+    private LocalTime horaFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReservationStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
+
+    private void prePersist(){
+        this.status = ReservationStatus.AGENDADA;
+        this.dataCriacao = LocalDateTime.now();
+    }
+}
