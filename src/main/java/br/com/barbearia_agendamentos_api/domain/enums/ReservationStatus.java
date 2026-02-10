142bc985b6beb42b9f6b53bb44f367e13f5bb0e2
João Victor <joao.dev06@gmail.com>
2026-02-10 00:09:08 -0300

    feat: adiciona enums de dominio

---
+++ src/main/java/br/com/barbearia_agendamentos_api/domain/enums/ReservationStatus.java
@@ -0,0 +1,7 @@
+package br.com.barbearia_agendamentos_api.domain.enums;
+
+public enum ReservationStatus {
+    AGENDADA,
+    CANCELADA,
+    CONCLUIDA
+}
---
+++ src/main/java/br/com/barbearia_agendamentos_api/domain/enums/Role.java
@@ -0,0 +1,7 @@
+package br.com.barbearia_agendamentos_api.domain.enums;
+
+public enum Role {
+    CLIENTE,
+    BARBEIRO,
+    ADMIN
+}
