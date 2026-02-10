10559f173aa436b7a4c41ec1066f753996b54a26
João Victor <joao.dev06@gmail.com>
2026-02-10 13:06:51 -0300

    feat: adiciona ReservationRepository com verificação de conflito

---
+++ src/main/java/br/com/barbearia_agendamentos_api/repository/ReservationRepository.java
@@ -0,0 +1,25 @@
+package br.com.barbearia_agendamentos_api.repository;
+
+import br.com.barbearia_agendamentos_api.domain.entity.Reservation;
+import br.com.barbearia_agendamentos_api.domain.enums.ReservationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByBarberIdAndDataAndStatus(
+            Long barberId,
+            LocalDate data,
+            ReservationStatus status
+    );
+
+    boolean existsByBarberIdAndDataAndHoraInicioLessThanAndHoraFimGreaterThan(
+            Long barberId,
+            LocalDate data,
+            LocalTime horaFim,
+            LocalTime horaInicio
+    );
+}
