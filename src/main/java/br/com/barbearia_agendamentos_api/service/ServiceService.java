fe93cedab590ff52f5250907a014a2b7f75fece5
João Victor <joao.dev06@gmail.com>
2026-02-11 11:39:44 -0300

    refactor: atualiza ServiceService para utilizar dto

--- src/main/java/br/com/barbearia_agendamentos_api/service/ServiceService.java
+++ src/main/java/br/com/barbearia_agendamentos_api/service/ServiceService.java
@@ -2,11 +2,15 @@ package br.com.barbearia_agendamentos_api.service;
 
 import br.com.barbearia_agendamentos_api.domain.entity.BarberService;
 import br.com.barbearia_agendamentos_api.domain.exception.ResourceNotFoundException;
+import br.com.barbearia_agendamentos_api.domain.mapper.ServiceMapper;
+import br.com.barbearia_agendamentos_api.dto.service.ServiceRequest;
+import br.com.barbearia_agendamentos_api.dto.service.ServiceResponse;
 import br.com.barbearia_agendamentos_api.repository.ServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,17 +18,25 @@ public class ServiceService {
 
     private final ServiceRepository serviceRepository;
 
-    public BarberService create(BarberService service){
-        service.setAtivo(true);
-        return serviceRepository.save(service);
+    public ServiceResponse create(ServiceRequest request){
+        BarberService service = BarberService.builder()
+                .nome(request.getNome())
+                .descricao(request.getDescricao())
+                .duracaoMinutos(request.getDuracaoMinutos())
+                .preco(request.getPreco())
+                .ativo(true)
+                .build();
+
+        serviceRepository.save(service);
+
+        return ServiceMapper.toResponse(service);
     }
 
-    public List<BarberService> activeList(){
-        return serviceRepository.findByAtivoTrue();
+    public List<ServiceResponse> findAllActive() {
+        return serviceRepository.findByAtivoTrue()
+                .stream()
+                .map(ServiceMapper::toResponse)
+                .collect(Collectors.toList());
     }
 
-    public Service findServiceById(Long id){
-        return (Service) serviceRepository.findById(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Serviço não encontrado"));
-    }
 }
