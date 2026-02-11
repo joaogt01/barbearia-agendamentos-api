b904ca357db12fa8156387676053e259618235b5
João Victor <joao.dev06@gmail.com>
2026-02-10 22:26:55 -0300

    refactor: atualiza UserService para utilizar dto

--- src/main/java/br/com/barbearia_agendamentos_api/service/UserService.java
+++ src/main/java/br/com/barbearia_agendamentos_api/service/UserService.java
@@ -2,9 +2,15 @@ package br.com.barbearia_agendamentos_api.service;
 
 import br.com.barbearia_agendamentos_api.domain.entity.User;
 import br.com.barbearia_agendamentos_api.domain.exception.ResourceNotFoundException;
+import br.com.barbearia_agendamentos_api.domain.mapper.UserMapper;
+import br.com.barbearia_agendamentos_api.dto.user.UserRequest;
+import br.com.barbearia_agendamentos_api.dto.user.UserResponse;
 import br.com.barbearia_agendamentos_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,17 +18,42 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User create(User user){
-        return userRepository.save(user);
+    public UserResponse create(UserRequest request){
+        if (userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Email já cadastrado.");
+        }
+
+        User user = User.builder()
+                .nome(request.getNome())
+                .email(request.getEmail())
+                .senha(request.getSenha())
+                .role(request.getRole())
+                .ativo(true)
+                .build();
+
+        userRepository.save(user);
+
+        return UserMapper.toResponse(user);
     }
 
-    public User findUserById(Long id){
-        return userRepository.findById(id)
+    public UserResponse findUserById(Long id){
+        User user = userRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Usuário não encontrado"));
+
+        return UserMapper.toResponse(user);
     }
 
-    public User findUserByEmail(String email){
-        return userRepository.findByEmail(email)
+    public UserResponse findUserByEmail(String email){
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(()-> new ResourceNotFoundException("Usuário não encontrado"));
+
+        return UserMapper.toResponse(user);
+    }
+
+    public List<UserResponse> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::toResponse)
+                .collect(Collectors.toList());
     }
 }
